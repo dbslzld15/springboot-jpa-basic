e8c69fae05d5119d2e6fabb023a2c70286f2daf8
dbslzld15 <dbslzld15@naver.com>
2022-02-19 23:00:27 +0900

    패키지 구조 및 필드 수정

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,14 +6,16 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
-public class Address {
+public class Address { // 값 타입은 변경 불가능하게 설계해야함.
+    // Setter를 제거하고 생성자에서 값을 모두 초기화해서 변경 불가능한 클래스를 만들자
 
     private String city;
     private String street;
     private String zipcode;
 
-    protected Address() {
-    }
+    //jpa 스팩상 만들어줌
+    protected Address() { // jpa에서 리플랙션이나 프록시같은 기술을 사용해 엔티티를 생성할때가 많은데 이를 위해 기본 생성자를 만들어줌
+    } // public은 여러 사람들이 사용할 수 있으니 protected로 사용
 
     public Address(String city, String street, String zipcode) {
         this.city = city;
--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.member.Member;
+import jpabook.jpashop.domain.member.MemberRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class) // Spring과 관련된 테스트를 한다고 Junit에 알려줌
 @SpringBootTest
 public class MemberRepositoryTest {
 
@@ -19,18 +21,18 @@ public class MemberRepositoryTest {
 
     @Test
     @Transactional
-    @Rollback(value = false)
+    @Rollback(value = false)  //롤백 안하면 데이터가 남아 있음
     public void testMember(){
         //given
         Member member = new Member();
-        member.setUsername("memberA");
+        member.setName("memberA");
         //when
         Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
 
         //then
-        Member findMember = memberRepository.find(savedId);
         assertThat(findMember.getId()).isEqualTo(member.getId());
-        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember.getName()).isEqualTo(member.getName());
         assertThat(findMember).isEqualTo(member);
     }
 
