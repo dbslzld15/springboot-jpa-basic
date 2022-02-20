33c2b9a68593d7e373c79e9172990e8071e93eea
dbslzld15 <dbslzld15@naver.com>
2022-02-20 21:59:23 +0900

    회원 저장소 코드 추가

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -19,21 +19,6 @@ public class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
-    @Test
-    @Transactional
-    @Rollback(value = false)  //롤백 안하면 데이터가 남아 있음
-    public void testMember(){
-        //given
-        Member member = new Member();
-        member.setName("memberA");
-        //when
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
 
-        //then
-        assertThat(findMember.getId()).isEqualTo(member.getId());
-        assertThat(findMember.getName()).isEqualTo(member.getName());
-        assertThat(findMember).isEqualTo(member);
-    }
 
 }
