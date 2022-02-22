0568bc201e33c46a28d5116ea45a0969cc36b545
dbslzld15 <dbslzld15@naver.com>
2022-02-22 22:36:21 +0900

    주문 서비스 소스 코드 추가

--- src/main/java/jpabook/jpashop/domain/order/Order.java
+++ src/main/java/jpabook/jpashop/domain/order/Order.java
@@ -3,7 +3,9 @@ package jpabook.jpashop.domain.order;
 import jpabook.jpashop.domain.delivery.Delivery;
 import jpabook.jpashop.domain.delivery.DeliveryStatus;
 import jpabook.jpashop.domain.member.Member;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -14,6 +16,7 @@ import java.util.List;
 @Entity
 @Table(name = "orders")
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id
