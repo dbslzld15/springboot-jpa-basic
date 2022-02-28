c123c1d1011e1ecaaf4455578454a5a5214732a4
dbslzld15 <dbslzld15@naver.com>
2022-02-28 21:44:57 +0900

    주문 목록 검색, 취소 로직 추가

--- src/main/java/jpabook/jpashop/domain/order/OrderController.java
+++ src/main/java/jpabook/jpashop/domain/order/OrderController.java
@@ -7,9 +7,7 @@ import jpabook.jpashop.domain.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,11 +30,25 @@ public class OrderController {
         return "order/orderForm";
     }
 
-    @PostMapping("/orders")
+    @PostMapping("/order")
     public String order(@RequestParam("memberId") Long memberId,
                         @RequestParam("itemId") Long itemId,
                         @RequestParam("count") int count) {
         orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
         return "redirect:/orders";
     }
 }
