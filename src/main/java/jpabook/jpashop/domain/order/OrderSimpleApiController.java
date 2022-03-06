1a2aade3651dff11a287fe02b90d934a1c7f0ea9
dbslzld15 <dbslzld15@naver.com>
2022-03-06 15:29:28 +0900

    feat: 주문 조회 V2, 엔티티를 DTO로 변환하지만 N+1문제 발생 예제 추가

--- src/main/java/jpabook/jpashop/domain/order/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/domain/order/OrderSimpleApiController.java
@@ -1,10 +1,16 @@
 package jpabook.jpashop.domain.order;
 
+import jpabook.jpashop.domain.address.Address;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne(ManyToOne, OneToOne) 에서의 성능 최적화)
@@ -27,4 +33,38 @@ public class OrderSimpleApiController {
         return all;
     }
 
+    @GetMapping("/api/v2/simple-orders")
+    public Result ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        // N+1 문제가 발생, Order 쿼리 1개를 위해 회원 N + 배송 N 개의 추가 쿼리 발생
+       List<SimpleOrderDto> collect = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
 }
