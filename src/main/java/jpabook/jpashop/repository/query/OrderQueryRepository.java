925e52fa96062df2bbef94878c8d0fa030f5e180
dbslzld15 <dbslzld15@naver.com>
2022-03-07 14:52:55 +0900

    feat: 주문 조회 V5, JPA에서 DTO 직접 조회(컬렉션 조회 최적화)

--- src/main/java/jpabook/jpashop/repository/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/query/OrderQueryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -44,4 +46,36 @@ public class OrderQueryRepository {
         ).getResultList();
     }
 
+    public List<OrderQueryDto> findAllByDtoOptimization() {
+        List<OrderQueryDto> orders = findOrders();
+
+        List<Long> orderIds = toOrderIds(orders);
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(orderIds);
+
+        orders.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+        return orders;
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                        "select new jpabook.jpashop.repository.query.OrderItemQueryDto(" +
+                                "oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                                " from OrderItem oi" +
+                                " join oi.item i" +
+                                " where oi.order.id in :orderIds", OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream()
+                .collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));
+        return orderItemMap;
+    }
+
+    private List<Long> toOrderIds(List<OrderQueryDto> orders) {
+        List<Long> orderIds = orders.stream()
+                .map(o -> o.getOrderId())
+                .collect(Collectors.toList());
+        return orderIds;
+    }
 }
